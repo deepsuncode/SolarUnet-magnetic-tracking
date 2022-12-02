e5c44c99e36152fa1f46e35ee6df7bbf251c184f
Prajwal Mani <56246392+prajwalmani@users.noreply.github.com>
2022-12-02 03:03:54 -0500

    Update solarunet.py

--- solarunet.py
+++ solarunet.py
@@ -17,8 +17,8 @@
 
 import warnings
 import os
-import numpy as np
 import sys
+import numpy as np
 warnings.filterwarnings('ignore')
 import tensorflow as tf
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
